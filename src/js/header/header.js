0d9fb7b7c914e7fb133cd8ee3a5f20e7324dbeb3
Oleksii Hiriavenko <martmarchmartmarch@gmail.com>
2024-06-17 17:28:49 +0300

    update: header adaptive in progress

--- src/js/header/header.js
+++ src/js/header/header.js
@@ -1,1 +1,15 @@
+const burgerBtn = document.querySelector(".header-burger");
+const nav = document.querySelector("header nav");
 
+function toggleBurger(event) {
+  const target = event.target.closest("button");
+  if (target) {
+    if (nav.matches(".active")) {
+      nav.classList.remove("active");
+    } else {
+      nav.classList.add("active");
+    }
+  }
+}
+
+burgerBtn.addEventListener("click", toggleBurger);
